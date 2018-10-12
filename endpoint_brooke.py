1898c20090c8c854e942a0d9717a92deaf346668
Brooke Brown <brownb8@students.rowan.edu>
2018-10-12 19:14:55 -0400

    RESTful API that allows backend to connect frontend
    API calls methods from database API to test we can grab items from database

---
+++ endpoint_brooke.py
@@ -0,0 +1,20 @@
+import requests
+from db_api import get_user_by_id
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/hello")
+def hello():
+    return "Hello World!"
+
+@app.route("/test")
+def last_fm():
+    response = requests.get("http://ws.audioscrobbler.com/2.0/?method=user.getinfo&user=rj&api_key=8ed3258b37f9fb17b765bb7589e06c6f&format=json")
+
+#   print(response.status_code)
+    return response.text
+
+@app.route("/database")
+def user_info():
+    return get_user_by_id(1)
