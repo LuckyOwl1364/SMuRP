10242499ae59084d75a55f4feee3b4e2bb58e245
sharpevil <Sharpevil@gmail.com>
2018-10-14 15:58:43 -0400

    Due to issues with my previous approach, I updated to a more thorough ORM approach. Created classes and inits for all non-bridging tables.

--- backend/db_api.py
+++ backend/db_api.py
@@ -1,52 +1,59 @@
-import sqlalchemy
-from sqlalchemy import *
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 import datetime
 
-engine = sqlalchemy.create_engine('mysql+pymysql://admin:Talwkatgigasbas2h@ec2-52-91-42-119.compute-1.amazonaws.com:3306/smurp')
-Base = automap_base()
-Base.prepare(engine, reflect=True)
-Album = Base.classes.album
-# Album_featuring = Base.classes.album_featuring
-Artist = Base.classes.artist
-# Listened_To = Base.classes.listened_to
-# Rated = Base.classes.rated
-# Recommendation = Base.classes.recommendation
-Song = Base.classes.song
-# Song_By = Base.classes.song_by
-# Song_On = Base.classes.song_on
-User = Base.classes.user
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'mysql://admin:Talwkatgigasbas2h@ec2-52-91-42-119.compute-1.amazonaws.com:3306/smurp'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-session = Session(engine)
 
-def create_user(lastfm_name, username="", password="", email=""):
-    session.add(User(username=username, lastfm_name=lastfm_name, join_date=datetime.datetime.utcnow(),
-                     password=password, email=email))
-    session.commit()
+class Album(db.Model):
+    __tablename__ = 'album'
+    album_id = db.Column('album_id', db.Integer, primary_key=True)
+    album_name = db.Column('album_name', db.Unicode)
+    album_art = db.Column('album_art', db.Unicode)
 
-def create_artist(artist_name):
-    session.add(Artist(artist_name=artist_name))
-    session.commit()
+    def __init__(self, album_name, album_art=""):
+        self.album_name = album_name
+        self.album_art = album_art
 
-# def rate(user_id, song_id, positive):
-#     rating = 0
-#     if positive:
-#         rating += 1
-#     session.add(Rated(user_id=user_id, song_id=song_id, rated=rating))
-#     session.commit()
 
-# def recommendation(user_id, rec_id, rec_type):
-#     session.add(Recommendation(user_id=user_id, rec_id=rec_id, rec_type=rec_type))
-#     session.commit()
+class Artist(db.Model):
+    __tablename__ = 'artist'
+    artist_id = db.Column('artist_id', db.Integer, primary_key=True)
+    artist_name = db.Column('artist_name', db.Unicode)
 
-def create_song(song_title, spotify_id=""):
-    session.add(Song(song_title=song_title, spotify_id=spotify_id))
-    session.commit()
+    def __init__(self, artist_name):
+        self.artist_name = artist_name
 
-def create_album(album_name, album_art=""):
-    session.add(Album(album_name=album_name, album_art=album_art))
-    session.commit()
 
-def get_user_by_id(user_id):
-    return session.query(User).get(user_id)
+class Song(db.Model):
+    __tablename__ = 'song'
+    song_id = db.Column('song_id', db.Integer, primary_key=True)
+    song_title = db.Column('song_title', db.Unicode)
+    spotify_id = db.Column('spotify_id', db.Integer)
+
+    def __init__(self, song_title, spotify_id=""):
+        self.song_title = song_title
+        self.spotify_id = spotify_id
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+    user_id = db.Column('user_id', db.Integer, primary_key=True)
+    username = db.Column('username', db.Unicode)
+    lastfm_name = db.Column('lastfm_name', db.Unicode)
+    join_date = db.Column('join_date', db.DateTime)
+    password = db.Column('password', db.Unicode)
+    email = db.Column('email', db.Unicode)
+
+    def __init__(self, lastfm_name, username="", password="", email=""):
+        self.lastfm_name = lastfm_name
+        self.username = username
+        self.password = password
+        self.email = email
+        if username:
+        self.join_date = datetime.datetime.now()
+
