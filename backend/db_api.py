1bac8e225538ac96616c464556c59999fe8d426e
Sharpevil <Sharpevil@gmail.com>
2018-10-23 15:50:14 -0400

    Added all the functions necessary to populate with lastfm data. Not thoroughly tested.

--- backend/db_api.py
+++ backend/db_api.py
@@ -1,9 +1,14 @@
-from backend.__init__ import create_app, db
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 import sqlalchemy.exc
 import datetime
 import json
 
-app = create_app()
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'mysql://admin:Talwkatgigasbas2h@ec2-52-91-42-119.compute-1.amazonaws.com:3306/smurp'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 album_featuring = db.Table('album_featuring',
                            db.Column('artist_id', db.Integer, db.ForeignKey('artist.artist_id')),
@@ -109,6 +114,34 @@ class Rated(db.Model):
 # TODO: Recommendation Table.
 # If possible, fix lack of foreign key implementation through sqlalchemy
 
+def get_album_by_id(album_id):
+    album = db.session.query(Album).get(album_id)
+    album_dict = {
+        "album_id": album.album_id,
+        "album_name": album.album_name,
+        "album_art": album.album_art
+    }
+    return json.dumps(album_dict)
+
+
+def get_artist_by_id(artist_id):
+    artist = db.session.query(Artist).get(artist_id)
+    artist_dict = {
+        "artist_id": artist.artist_id,
+        "artist_name": artist.artist_name
+    }
+    return json.dumps(artist_dict)
+
+
+def get_song_by_id(song_id):
+    song = db.session.query(Song).get(song_id)
+    song_dict = {
+        "song_id": song.song_id,
+        "song_title": song.song_title,
+        "spotify_id": song.spotify_id
+    }
+    return json.dumps(song_dict)
+
 
 def get_user_by_id(user_id):
     user = db.session.query(User).get(user_id)
@@ -127,8 +160,103 @@ def add_lastfm_user(fm_username):
     db.session.add(user)
     try:
         db.session.commit()
-        return User.user_id
+        return user.user_id
     except sqlalchemy.exc.IntegrityError:
         db.session.rollback()
         return "ERROR: Could not add user: " + fm_username
+
+
+def add_song(song_title, spotify_id=None):
+    song = Song(song_title, None, spotify_id)
+    db.session.add(song)
+    try:
+        db.session.commit()
+        return song.song_id
+    except sqlalchemy.exc.IntegrityError:
+        db.session.rollback()
+        return "ERROR: Could not add song: " + song_title
+
+
+def add_artist(artist_name):
+    artist = Artist(artist_name)
+    db.session.add(artist)
+    try:
+        db.session.commit()
+        return artist.artist_id
+    except sqlalchemy.exc.IntegrityError:
+        db.session.rollback()
+        return "ERROR: Could not add artist: " + artist_name
+
+
+def add_album(album_name, album_art):
+    album = Album(album_name, album_art)
+    db.session.add(album)
+    try:
+        db.session.commit()
+        return album.album_id
+    except sqlalchemy.exc.IntegrityError:
+        db.session.rollback()
+        return "ERROR: Could not add album: " + album_name
+
+
+def add_song_by(song_id, artist_id):
+    song = db.session.query(Song).get(song_id)
+    artist = db.session.query(Artist).get(artist_id)
+    artist.performs.append(song)
+    try:
+        db.session.commit()
+        return "Success"
+    except sqlalchemy.exc.IntegrityError:
+        db.session.rollback()
+        return "ERROR: Could not create relationship."
+
+
+def add_song_on(song_id, album_id):
+    song = db.session.query(Song).get(song_id)
+    album = db.session.query(Album).get(album_id)
+    album.song_on.append(song)
+    try:
+        db.session.commit()
+        return "Success"
+    except sqlalchemy.exc.IntegrityError:
+        db.session.rollback()
+        return "ERROR: Could not create relationship."
+
+
+def add_album_featuring(album_id, artist_id):
+    album = db.session.query(Album).get(album_id)
+    artist = db.session.query(Artist).get(artist_id)
+    artist.featured_on.append(album)
+    try:
+        db.session.commit()
+        return "Success"
+    except sqlalchemy.exc.IntegrityError:
+        db.session.rollback()
+        return "ERROR: Could not create relationship."
+
+
+def add_listened_to(user_id, song_id):
+    listen = db.session.query(ListenedTo).filter_by(
+        user_id = user_id,
+        song_id = song_id
+    ).first()
+
+    if listen:
+        listen.num_listens += 1
+        listen.last_listen = datetime.datetime.now()
+        try:
+            db.session.commit()
+            return "Successfully updated"
+        except sqlalchemy.exc.IntegrityError:
+            db.session.rollback()
+            return "ERROR: Could not update listen"
+    else:
+        listen = ListenedTo(user_id, song_id)
+        db.session.add(listen)
+        try:
+            db.session.commit()
+            return "Successfully added"
+        except sqlalchemy.exc.IntegrityError:
+            db.session.rollback()
+            return "ERROR: Could not add listen"
 
