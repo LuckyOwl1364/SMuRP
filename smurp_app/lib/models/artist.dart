79a1a7bee3c3166d327ff66aca44f18eb5f10a4c
Caitlin☼ <NbtK5PlPg79s>
2018-10-25 17:08:58 -0400

    Split 'classes' into 'artist' and 'song'

---
+++ smurp_app/lib/models/artist.dart
@@ -0,0 +1,6 @@
+class Artist{
+  final String name;
+  final String genre;
+
+  Artist(this.name,this.genre);
+}
---
+++ smurp_app/lib/models/song.dart
@@ -0,0 +1,8 @@
+import 'package:smurp_app/models/artist.dart';
+
+class Song{
+  final String title;
+  final Artist artist;
+
+  Song(this.title, this.artist);
+}
