0a82782d4ba0f7f70be3fe6b5f7eb9c7c57a0fe6
Caitlin☼ <NbtK5PlPg79s>
2018-10-25 17:10:31 -0400

    Added 'rest_ds", which handles interacting with REST endpoints

---
+++ smurp_app/lib/data/rest_ds.dart
@@ -0,0 +1,24 @@
+// This file inspired by: https://medium.com/@kashifmin/flutter-login-app-using-rest-api-and-sqflite-b4815aed2149
+
+import 'dart:async';
+
+import 'package:smurp_app/utils/network_util.dart';
+import 'package:smurp_app/models/user.dart';
+
+class RestDatasource {
+  NetworkUtil _netUtil = new NetworkUtil();
+  static final BASE_URL = "http://YOUR_BACKEND_IP/login_app_backend";
+  static final LOGIN_URL = BASE_URL + "/login.php";
+  static final _API_KEY = "somerandomkey";
+
+  Future<User> login(String username, String password) {
+    return _netUtil.post(LOGIN_URL, body: {
+      "username": username,
+      "password": password
+    }).then((dynamic res) {
+      print(res.toString());
+      if(res["error"]) throw new Exception(res["error_msg"]);
+      return new User.map(res["user"]);
+    });
+  }
+}
