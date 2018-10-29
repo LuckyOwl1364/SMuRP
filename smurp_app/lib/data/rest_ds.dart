5834af6b39b48548765daa6d4eddbea0398f6a61
Caitlin☼ <NbtK5PlPg79s>
2018-10-29 11:34:49 -0400

    Got rid of using an API key for logging in or doing other things
    If it is wanted back I can put it back that's fine

--- smurp_app/lib/data/rest_ds.dart
+++ smurp_app/lib/data/rest_ds.dart
@@ -9,7 +9,6 @@ class RestDatasource {
   NetworkUtil _netUtil = new NetworkUtil();
   static final BASE_URL = "http://YOUR_BACKEND_IP/login_app_backend";
   static final LOGIN_URL = BASE_URL + "/login.php";
-  static final _API_KEY = "somerandomkey";
 
   Future<User> login(String username, String password) {
     return _netUtil.post(LOGIN_URL, body: {
