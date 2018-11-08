e2e795b75a950cf3b8721a70939e0db28a26f40e
Caitlin☼ <NbtK5PlPg79s>
2018-11-08 10:41:07 -0500

    Minor edits to 'rest_ds'; moved generic getData() method into 'rest_ds'

--- smurp_app/lib/data/rest_ds.dart
+++ smurp_app/lib/data/rest_ds.dart
@@ -1,6 +1,8 @@
 // This file inspired by: https://medium.com/@kashifmin/flutter-login-app-using-rest-api-and-sqflite-b4815aed2149
 
 import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 import 'package:smurp_app/utils/network_util.dart';
 import 'package:smurp_app/models/user.dart';
@@ -8,9 +10,9 @@ import 'package:smurp_app/models/song.dart';
 
 class RestDatasource {
   NetworkUtil _netUtil = new NetworkUtil();
-  static final BASE_URL = "https://ec2-52-91-42-119.compute-1.amazonaws.com:5000";
-  static final LOGIN_URL = BASE_URL + "/login";
-  static final ONESONG_URL = BASE_URL + "/get_song";
+  static final BASE_URL = "http://ec2-52-91-42-119.compute-1.amazonaws.com:5000/";
+  static final LOGIN_URL = BASE_URL + "login";
+  static final ONESONG_URL = BASE_URL + "get_song";
 
   Future<User> login(String username, String password) {
     return _netUtil.post(LOGIN_URL, body: {
@@ -19,7 +21,7 @@ class RestDatasource {
     }).then((dynamic res) {
       print(res.toString());
       if(res["error"]) throw new Exception(res["error_msg"]);
-      return new User.map(res["user"]);
+      return new User.fromJson(res["user"]);
     });
   }
 
@@ -27,9 +29,23 @@ class RestDatasource {
     return _netUtil.get(ONESONG_URL)
         .then((dynamic res) {
           print(res.toString());
-          if(res["error"]) throw new Exception(res["error_msg"]);
+//          if(res["error"]) throw new Exception(res["error_msg"]);
           return new Song.map(res["song"]);
         });
+  }
+
+
+  //async call to get data from endpoint
+  Future<User> getData() async {
+    http.Response response = await http.get(
+        BASE_URL + "database",
+        headers: {
+          "Accept": "application/json"
+        }
+    );
+    Map userMap = json.decode(response.body);
+    var user = new User.fromJson(userMap);
+    return user;
   }
 
 }
