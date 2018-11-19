e92257955038c1a1a706e1585fb6184f5baea16d
Caitlin☼ <NbtK5PlPg79s>
2018-11-19 14:36:21 -0500

    User objects in the app no longer get to know about their emails or passwords

--- smurp_app/lib/models/user.dart
+++ smurp_app/lib/models/user.dart
@@ -6,37 +6,28 @@ class User{
   final String username;
   final String lastfm_name;
   final String join_date; //DateTime object?
-  final String password;
-  final String email;
 
-  User(this.user_id, this.username, this.lastfm_name, this.join_date,
-      this.password, this.email);
+  User(this.user_id, this.username, this.lastfm_name, this.join_date);
 
   User.fromJson(Map<Object, dynamic> json)
       : user_id = json['user_id'],
         lastfm_name = json['lastfm_name'],
         join_date = json['join_date'], //DateTime object?
-        password = json['password'],
-        username = json['username'],
-        email = json['email'];
+        username = json['username'];
 
   Map<Object, dynamic> toJson() =>
       {
         'user_id' : user_id,
         'lastfm_name' : lastfm_name,
         'join_date' : join_date,
-        'password' : password,
         'username': username,
-        'email': email,
       };
 
   String toString(){
     String values = "GRAB DATA FROM USER: \n"
         + "lastfm name: " + lastfm_name + '\n'
         + "join date: " + join_date + '\n'
-        + "password: " + password + '\n'
-        + "username: " + username + '\n'
-        + "email: " + email + '\n';
+        + "username: " + username + '\n';
 
     return values;
   }
