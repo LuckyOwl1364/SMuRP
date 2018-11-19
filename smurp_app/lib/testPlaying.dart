cc7ecaf90ccef2efb01510523b1bb40acb9c6780
Caitlin☼ <NbtK5PlPg79s>
2018-11-19 15:11:43 -0500

    Renamed 'following' to 'testPlaying' so it doesn't get overwritten and to make clear what I have been using it for

--- smurp_app/lib/testPlaying.dart
+++ smurp_app/lib/testPlaying.dart
@@ -5,6 +5,8 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:smurp_app/models/user.dart';
 
+import 'package:smurp_app/data/rest_ds.dart';
+
 void main() {
   runApp(new MaterialApp(
     home: new FollowingPage(),
@@ -21,21 +23,22 @@ class FollowingPageState extends State<FollowingPage> {
   String endPtData = "Test Data ";
   List data;
 
+  final RestDatasource restDS = new RestDatasource();
+
   //async call to get data from endpoint
   Future<String> getData() async{
-    http.Response response = await http.get("http://ec2-52-91-42-119.compute-1.amazonaws.com:5000/database",
-        headers: {
-          "Accept" : "application/json"
-        }
-    );
+    var user = await restDS.getData();//login("theactualdevil","good_password");
+
 
     setState((){
-      Map userMap = json.decode(response.body);
-      var user = new User.fromJson(userMap);
+//      Map userMap = json.decode(responseBody);
+//      var user = new User.fromJson(userMap);
 
       endPtData = 'DATA RECIEVED FROM ENDPOINT\n'
-          'User name: ${user.username}\n'
-          'LastFM name: ${user.lastfm_name}\n';
+//          'User name: ${user.username}\n'
+//          'LastFM name: ${user.lastfm_name}\n'
+//          'User ID: ${user.user_id}';
+          '$user';
 
       //data.add('User name is ${user.username} \n LastFM name is ${user.lastfm_name}');
       print(endPtData);
