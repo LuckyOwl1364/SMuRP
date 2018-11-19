dfba7306df496c6e92e066a2fb16a5ee89f8c317
Caitlin☼ <NbtK5PlPg79s>
2018-11-19 10:02:13 -0500

    Revert "Merge branch 'frontend' of https://github.com/LuckyOwl13/SMuRP into frontend"
    This reverts commit d195405cd6cf4c6b9c53559f35ce8fd3bd9aa866, reversing
    changes made to 2c4599a55eaae0dbd28f90c294508a34073f2028.

--- smurp_app/lib/following.dart
+++ smurp_app/lib/following.dart
@@ -3,9 +3,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:smurp_app/history.dart';
 import 'package:smurp_app/models/user.dart';
-import 'package:smurp_app/utils/endpointDemo.dart';
 
 void main() {
   runApp(new MaterialApp(
@@ -19,30 +17,34 @@ class FollowingPage extends StatefulWidget {
 }
 
 class FollowingPageState extends State<FollowingPage> {
+
   String endPtData = "Test Data ";
   List data;
-  double regularPadding = 8.0;
-  double halfPadding = 4.0;
-  double doublePadding = 16.0;
 
   //async call to get data from endpoint
-  Future<String> getData() async {
-    http.Response response = await http.get(
-        "http://ec2-52-91-42-119.compute-1.amazonaws.com:5000/database",
-        headers: {"Accept": "application/json"});
+  Future<String> getData() async{
+    http.Response response = await http.get("http://ec2-52-91-42-119.compute-1.amazonaws.com:5000/database",
+        headers: {
+          "Accept" : "application/json"
+        }
+    );
 
-    setState(() {
+    setState((){
       Map userMap = json.decode(response.body);
       var user = new User.fromJson(userMap);
 
-//      endPtData = 'DATA RECIEVED FROM ENDPOINT\n'
-//          'User name: ${user.username}\n'
-//          'LastFM name: ${user.lastfm_name}\n';
+      endPtData = 'DATA RECIEVED FROM ENDPOINT\n'
+          'User name: ${user.username}\n'
+          'LastFM name: ${user.lastfm_name}\n';
 
-      endPtData = '${user.username}';
-
+      //data.add('User name is ${user.username} \n LastFM name is ${user.lastfm_name}');
       print(endPtData);
+      //data.add(user);
     });
+
+    //data.add('Some Generic user name is abcUser321 \n generic LastFM name is abcU321');
+    //data.add('Another Generic username is 432wxy \n another generic LastFM name is wxy432');
+
   }
 
   @override
@@ -55,113 +57,17 @@ class FollowingPageState extends State<FollowingPage> {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text("Following"),
-      ),
-      drawer: new Drawer(
-        child: ListView(
-          children: <Widget>[
-            UserAccountsDrawerHeader(
-                accountName: Text(endPtData),
-                accountEmail: Text('hardCodedEmail@email.com'),
-                decoration: BoxDecoration(
-                  color: Colors.lightBlue,
-                )),
-            ListTile(
-              contentPadding: EdgeInsets.symmetric(
-                  vertical: halfPadding, horizontal: doublePadding),
-              leading: const Icon(Icons.account_circle),
-              title: Text('Profile'),
-              onTap: () {
-                // ignore: return_of_invalid_type_from_closure
-                Navigator.push(
-                    context,
-                    new MaterialPageRoute(
-                        builder: (context) => new EndpointDemoPage()));
-              }, //code to navigate to appropriate screen
-            ),
-            ListTile(
-              contentPadding: EdgeInsets.symmetric(
-                  vertical: halfPadding, horizontal: doublePadding),
-              leading: const Icon(Icons.stars),
-              title: Text('Recommended'),
-              onTap: () {
-                Navigator.push(
-                    context,
-                    new MaterialPageRoute(
-                        builder: (context) => new EndpointDemoPage()));
-              }, //code to navigate to appropriate screen
-            ),
-            ListTile(
-              contentPadding: EdgeInsets.symmetric(
-                  vertical: halfPadding, horizontal: doublePadding),
-              leading: const Icon(Icons.thumbs_up_down),
-              title: Text('Rated Songs'),
-              onTap: () {
-                Navigator.push(
-                    context,
-                    new MaterialPageRoute(
-                        builder: (context) => new EndpointDemoPage()));
-              }, //code to navigate to appropriate screen
-            ),
-            ListTile(
-              contentPadding: EdgeInsets.symmetric(
-                  vertical: halfPadding, horizontal: doublePadding),
-              leading: const Icon(Icons.history),
-              title: Text('History'),
-              onTap: () {
-                Navigator.push(context,
-                    new MaterialPageRoute(builder: (context) => new MyApp()));
-              }, //code to navigate to appropriate screen
-            ),
-            ListTile(
-              contentPadding: EdgeInsets.symmetric(
-                  vertical: halfPadding, horizontal: doublePadding),
-              leading: const Icon(Icons.account_circle),
-              title: Text('Log Out'),
-              onTap: () {
-                Navigator.push(
-                    context,
-                    new MaterialPageRoute(
-                        builder: (context) => new EndpointDemoPage()));
-              }, //code to navigate to appropriate screen
-            ),
-          ],
-        ),
+        title: new Text("Listviews"),
       ),
       body: new ListView.builder(
         itemCount: 1,
         itemBuilder: (BuildContext context, int index) {
           return new Card(
-            child: Row(
-              mainAxisSize: MainAxisSize.max,
-              children: <Widget>[
-                Expanded(
-                    child: Padding(
-                  padding: new EdgeInsets.all(regularPadding),
-                  child: new Text(endPtData, textAlign: TextAlign.start),
-                )),
-                Padding(
-                  padding: new EdgeInsets.symmetric(
-                      horizontal: regularPadding, vertical: halfPadding),
-                  child: RaisedButton(
-                    onPressed: unfollow,
-                    child: const Text('Unfollow'),
-                    color: Colors.lightBlue,
-                    textColor: Colors.white,
-                  ),
-                )
-              ],
-            ),
+            child: new Text(endPtData),
           );
         },
       ),
     );
-  } //build
-
-  void unfollow() {
-    setState(() {
-      //hit the endpoint
-      endPtData += " Unfollowed";
-    });
   }
-} //FollowingPageState
+
+}
