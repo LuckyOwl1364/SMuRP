635e6e8058d62856fcf2e0cefad88a82efad3c1b
Kristelle Lucero <kjmlucero@gmail.com>
2018-12-03 13:52:11 -0500

    oops forgot to add the globals file

---
+++ smurp_app/lib/globals.dart
@@ -0,0 +1,10 @@
+library my_prj.globals;
+
+bool isLoggedIn = false;
+int user_id = 323;
+
+final double regularPadding = 8.0;
+final double halfPadding = 4.0;
+final double doublePadding = 16.0;
+
+//todo add session information
