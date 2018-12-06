6128a448d7ff6fb70358b7a8d4549b12b8e8e5eb
brookebrown <brownb8@students.rowan.edu>
2018-12-06 11:26:49 -0500

    took out unnesscary import and test endpoints

--- backend/rest_api.py
+++ backend/rest_api.py
@@ -9,20 +9,6 @@ app.config.update(
     SESSION_COOKIE_PATH = '/'
 )
 
-#imports app instance from database api
-from db_api import app 
-
-@app.route("/hello")
-def hello():
-    return "Hello World!"
-
-@app.route("/test")
-def last_fm():
-    response = requests.get("http://ws.audioscrobbler.com/2.0/?method=user.getinfo&user=rj&api_key=8ed3258b37f9fb17b765bb7589e06c6f&format=json")
-
-#   print(response.status_code)
-    return response.text
-
 @app.route("/database")
 def user_info():
     return get_user_by_id(1)
