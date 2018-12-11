a6a08acdc05c6177bbc4b72bb9303d3fdd9a82fa
Caitlin☼ <NbtK5PlPg79s>
2018-12-11 16:52:43 -0500

    Removed an unused import and added a testing print call

--- smurp_app/lib/feed.dart
+++ smurp_app/lib/feed.dart
@@ -10,7 +10,6 @@ import 'package:smurp_app/friends.dart';
 import 'package:smurp_app/profile.dart';
 import 'package:smurp_app/recommended.dart';
 import 'package:smurp_app/data/rest_ds.dart';
-import 'package:smurp_app/main.dart';
 import 'globals.dart' as globals;
 
 
@@ -215,6 +214,7 @@ class FeedState extends State<FeedPage> {
       feedData = 'Successfully grabbed some data!';
 
       print(feedData);
+      print(feedList);
     });
   }
 
