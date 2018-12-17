e52e95d6711b3037ee0ce2ce33e4a1bc09b1f22d
Caitlin☼ <NbtK5PlPg79s>
2018-12-17 12:35:25 -0500

    'globals' commented

--- smurp_app/lib/globals.dart
+++ smurp_app/lib/globals.dart
@@ -12,9 +12,3 @@ int friend_id; //this user id is specific to opening a separate user's profile
 final double regularPadding = 8.0;
 final double halfPadding = 4.0;
 final double doublePadding = 16.0;
-
-//"username": "TheActualDevil",
-//    "lastfm_name": "DeadMetal1m1",
-//    "join_date": "November 13, 2018",
-//    "user_id": 323
-//todo add session information
